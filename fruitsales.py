6338c061d1360db652c33001198519bd8016904e
Aaron Ogle <aaronogle7@gmail.com>
2024-04-25 18:44:54 -0400

    Adjustments

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,14 @@
 import pandas as pd
+"""
+This imports the pandas library (module)
 
-data = pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]})
+Creates a dataframe that is then thrown into a csv at the place the script is ran.
+
+"""
+
+data = pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},index=['2017 Sales','2018 Sales'])
 
 data.to_csv("fruit.csv")
+
+print(data)
+
