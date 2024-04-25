6cef81d26faf04ed6a5082803b6c52adbf387300
Aaron Ogle <156380776+Aogle2@users.noreply.github.com>
2024-04-25 18:46:19 -0400

    Cleaned up and removed the print statement.

--- fruitsales.py
+++ fruitsales.py
@@ -9,6 +9,3 @@ Creates a dataframe that is then thrown into a csv at the place the script is ra
 data = pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},index=['2017 Sales','2018 Sales'])
 
 data.to_csv("fruit.csv")
-
-print(data)
-
